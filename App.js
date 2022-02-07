be515b97a4acf8da755b42e312096e27ddf1f3a1
Ella-Gotbring <54885982+Ella-Gotbring@users.noreply.github.com>
2022-02-07 20:28:09 +0100

    added navigation
    added and got navigation working in the project from home to async src

--- App.js
+++ App.js
@@ -1,23 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Async from "./src/Async.js";
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({ navigation }) {
+    return(
+        <View style={styles.container}>
+        <Text style={{marginBottom: '40px'}}>Projektarbete - Hybridutveckling</Text>
+        
+        <Button style={styles.button}
+        title="Press here to enter name"
+        onPress={() =>
+          navigation.navigate("Async")
+        }
+      />
+        </View>
+
+    )
+
+}
+
+
 export default function App() {
-    return ( <
-        View style = { styles.container } >
-        <
-        Async / >
-        <
-        StatusBar style = "auto" / >
-        <
-        /View>
-    );
+    const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Async" getComponent={() => require("./src/Async").default} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+ 
 });
